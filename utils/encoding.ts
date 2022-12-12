1ea85324aa913d16d06a8534489c8d239cc5a88b
Thunnini <yunjh1994@gmail.com>
2022-12-12 16:12:04 +0900

    Use `Buffer` package instead of Buffer on nodejs

--- utils/encoding.ts
+++ utils/encoding.ts
@@ -1,5 +1,7 @@
-export function base64URLEncode(str: Buffer) {
-  return str
+import { Buffer } from "buffer/";
+
+export function base64URLEncode(str: Uint8Array) {
+  return Buffer.from(str)
     .toString("base64")
     .replace(/\+/g, "-")
     .replace(/\//g, "_")
