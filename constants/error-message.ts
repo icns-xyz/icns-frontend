182b153a2620a9de6b1312b21419e469c25b37cd
Thunnini <yunjh1994@gmail.com>
2022-12-20 17:17:55 +0900

    Fix typo

--- constants/error-message.ts
+++ constants/error-message.ts
@@ -1,7 +1,7 @@
 export const TWITTER_LOGIN_ERROR = "Twitter login access denied";
 export const TWITTER_PROFILE_ERROR = "Twitter auth code is not valid";
 
-export const KEPLR_NOT_FOUND_ERROR = "Can't fount window.keplr";
+export const KEPLR_NOT_FOUND_ERROR = "Can't found window.keplr";
 export const KEPLR_VERSION_ERROR = "You should update keplr";
 
 export const KEPLR_NO_ACCOUNT_ERROR = "key doesn't exist";
