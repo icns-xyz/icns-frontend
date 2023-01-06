44b08084e51c29a72c88309ac41c7e36d5d24629
delivan <wjdgurdl272@gmail.com>
2023-01-06 16:55:16 +0900

    Change Sentry tracesSampleRate to 0.2

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -10,7 +10,7 @@ Sentry.init({
   enabled: !!SENTRY_DSN,
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.2,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
